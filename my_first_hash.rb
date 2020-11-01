7f60b22c7be97a22ab7e0f2c1e58987496df6712
mobinkakar <mobin.kakar@wsabronx.org>
2020-11-01 10:01:58 -0500

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,9 +17,10 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3 }
+  sm =shipping_manifest 
 
-  shipping_manifest["muskets"] = 2 , ["gun powder"] = 4
-  ["gun powder"] = 4
+  sm["muskets"] = 2 
+  sm["gun powder"] = 4
+  return sm
   
 end
